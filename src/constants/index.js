42079736477d0472836340850e97578f370fccb1
akash-az <akashranjan.ar020@gmail.com>
2024-11-27 01:07:35 +0530

    corrected tech stack

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,28 +14,28 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["Java", "SpringBoot", "ThymeLeaf", "MySql", "Bootstrap"],
+    technologies: ["Java", "SpringBoot", "MySql"],
   },
   {
     title: "Smart Contact Manager",
     image: project2,
     description:
       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "react", "nodejs","Firebase"],
+    technologies: [ "React", "nodejs","Firebase"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "ThymeLeaf"],
+    technologies: [ "React", "Tailwind"],
   },
   {
     title: "Blogging Platform",
     image: project4,
     description:
       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "reactjs","nodejs", "Express", "mySQL"],
+    technologies: ["reactjs","nodejs", "mySQL"],
   },
 ];
 
